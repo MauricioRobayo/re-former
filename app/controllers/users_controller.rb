daad320cea1d33d4da0cea0894b9f04142f21607
oscarnava <oscarnava@gmail.com>
2019-09-13 12:34:09 -0500

    Railsy-er Forms with #form_for

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,17 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  def new; end
+  def new
+    @user = User.new
+    @user.email = 'patito@patito.com'
+    warn "*************\n#{@user.inspect}\n***************"
+  end
 
   def create
     warn "\n\n>>> Create <<<\n#{params}\n\n"
     # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     @user = User.new(user_params)
+    warn "*************\n#{@user.inspect}\n***************"
     if @user.save
       redirect_to new_user_path
     else
@@ -18,6 +23,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.permit(:username, :email, :password)
+    params.require(:user).permit(:username, :email, :password)
   end
 end
