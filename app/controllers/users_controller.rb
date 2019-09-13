59265af85ef23cd1383ed06224813c28b8f28e88
Mauricio Robayo <rfmajo@gmail.com>
2019-09-13 11:29:55 -0500

    Use *_tag helpers instead of html tags

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :email, :password)
+    params.permit(:username, :email, :password)
   end
 end
