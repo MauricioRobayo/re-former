6ab22b5825d2691caf50d57e116df7a8281e2940
stickler-ci <support@stickler-ci.com>
2019-09-13 15:56:51 +0000

    Fixing style errors.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,16 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  def new
-  end
+  def new; end
 
   def create
-    STDERR.puts "\n\n>>> Create <<<\n#{params}\n\n"
+    warn "\n\n>>> Create <<<\n#{params}\n\n"
     # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     @user = User.new(user_params)
     if @user.save
       redirect_to new_user_path
     else
-      STDERR.puts "****** Can't save: #{@user.valid?}\n#{@user.errors.messages}\n\n"
+      warn "****** Can't save: #{@user.valid?}\n#{@user.errors.messages}\n\n"
       render :new
     end
   end
@@ -21,5 +20,4 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
-
 end
